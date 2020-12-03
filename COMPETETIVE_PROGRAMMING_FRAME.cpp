dab853f4bb01b5a4fc1773820b2ba2cd5bcd517b
MAXIMO278 <48401144+MAXIMO278@users.noreply.github.com>
2020-12-03 20:03:27 +0530

    Update COMPETETIVE_PROGRAMMING_FRAME.cpp

--- COMPETETIVE_PROGRAMMING_FRAME.cpp
+++ COMPETETIVE_PROGRAMMING_FRAME.cpp
@@ -1,123 +1,72 @@
-//CODER:::::::::MAXIMO278
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+
+
 using namespace std;
+using namespace __gnu_pbds;
+
 typedef long long ll;
-
-const int MOD = 1e9 + 7;
-
-//BFS GRAPH//
-//ll n;
-vector<ll> graph[250000];
-vector<bool> sta;
-queue<ll> st;
-void bfs(int n)
-{
-	cout<<n<<" ";
-	st.push(n);
-	sta[n]=true;
-	while(!st.empty()){
-		st.pop();
-	for(ll i=0;i<graph[n].size();i++)
-	{
-		if(sta[graph[n][i]]==false)
-		{
-			sta[graph[n][i]]=true;
-			bfs(graph[n][i]);
-		}
-	}
-}
-}
-
-//bfs()//
-//DFS TREE//
-int dfst(int x, int pred, int y)
-{
-   int mm = 0;
-
-   for (int i:v[x])
-   if (i!=pred){
-       int z = dfst(i, x, y);
-       mm = max(mm, z+1);
-   }
-
-   if (mm>=y || x==1) {ans++; mm = -1;}
-
-   return mm;
-}
-//int x=dfst()//
-
-
-//DFS GRAPH//
-void dfs(int n)
-{
-	cout<<n<<" ";
-	st.push(n);
-	sta[n]=true;
-	while(!st.empty()){
-		st.pop();
-	for(ll i=0;i<graph[n].size();i++)
-	{
-		if(sta[graph[n][i]]==false)
-		{
-			sta[graph[n][i]]=true;
-			dfs(graph[n][i]);
-		}
-	}
-}
-}
-
-//dfs()//
-
-
-//nCr MOD m//
-
- 
-int fact(ll a, ll b){
-    ll result = 1;
-    for(int i=0;i<b;++i){
-        result *= a;
-        a--;
-        result %= MOD;
-    }
-    return result;
-}
- 
-int mpow(ll x, ll n){
-    ll result = 1;
-    while( n != 0 ) {
-        if (n & 1) result = result*x % MOD;
-        x = x*x % MOD;
-        n = n >> 1;
-    }
-    return result % MOD;
-}
- 
-int combination(ll a, ll b) {
-    ll result = fact(a, b) % MOD;
-    result *= mpow(fact(b, b), MOD-2) % MOD;
-    return result % MOD;
-}
- 
- 
-//int x=combination(n,r) ;//nCr//
-
-
-//a^b mod m//
-
-ll modpow(ll base, ll exp) {
-  base %= MOD;
-  ll result = 1;
-  while (exp > 0) {
-    if (exp & 1) result = (result * base) % MOD;
-    base = (base * base) % MOD;
-    exp >>= 1;
-  }
-  return result%MOD;}
+/*
+ execution in cmd of windows: g++ p1.cpp -op1
   
-  //int x=modpow(a,b); //a^b %MOD//
+  
+  p1
+ 
+ */
+typedef long double dd;
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define nl <<"\n"
+#define con continue
+#define br break
+#define vll vector<ll>
+#define vpll vector<pair<ll,ll> >
+ll mx=INT_MAX;
+ll mi=INT_MIN;
+#define mem0(a)         memset(a,0,sizeof(a))
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define all(v) v.begin(), v.end()
+#define fsort(v) sort(all(v))  
+#define rsort(v) sort(v.rbegin(),v.rend())
+#define rev(v) reverse(all(v))
+#define maxc(v) max_element(all(v))
+#define minc(v) min_element(all(v))
+#define sumv(v,x) accumulate(v.begin(),v.end(),x);
+const ll mod = 1000000007;
+const ll inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+const ll MOD = 1e9+7;
+#define io { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define cer cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#define cs ll t;cin>>t;while(t--)
+#define cs1 ll t;t=1;while(t--)
+#define input(v,n) for(i=0;i<n;i++) cin>>v[i];
+#define output(v,n) for(i=0;i<n;i++) cout<<v[i]<<" ";
 
 int main()
 {
-	 ios_base::sync_with_stdio(false);
-  cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        ifstream cin("in.in");
+        ofstream cout("out.out");
+    #endif
+
+        io;
+        //////////////////////////////////
+        cs{
+             ll i,j,s=0,c,k=0,q=0,m=0,e=-1,f=-1,x,y,ans=INF,p=0,r,a,b,d,len,n;
+             
+
+      }
+      
+        
+        //////////////////////////////////
+        cer;
+            
+
 }
