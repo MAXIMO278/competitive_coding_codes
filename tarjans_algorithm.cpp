3ee3762053b02730deb35097ceaa278b2c8be577
MAXIMO278 <48401144+MAXIMO278@users.noreply.github.com>
2021-07-16 10:03:52 +0530

    Update tarjans_algorithm.cpp

--- tarjans_algorithm.cpp
+++ tarjans_algorithm.cpp
@@ -1,76 +1,62 @@
-
 class Solution
 {
 	public:
-	//Function to find number of strongly connected components in the graph.
-	typedef int ll;
-	stack<ll> st,df;
-	ll lv;
-	vector<ll> v,vv,nv;
-	
-	void dfs(ll n,vector<ll> adj[]){
-	    df.push(n);
-	    v[n]=1;
-	    while(df.size()>0){
-	        df.pop();
-	        for(ll i=0;i<adj[n].size();i++){
-	            if(v[adj[n][i]]==0){
-	                v[adj[n][i]]=1;
-	                dfs(adj[n][i],adj);
-	            }
-	        }
-	        st.push(n);
-	    }
-	}
-	
-	
-	void dfs2(ll n,vector<ll> adj[]){
-	    df.push(n);
-	    nv[n]=1;
-	    while(df.size()>0){
-	        df.pop();
-	        for(ll i=0;i<adj[n].size();i++){
-	            if(nv[adj[n][i]]==0){
-	                nv[adj[n][i]]=1;
-	                dfs2(adj[n][i],adj);
-	            }
-	        }
-	        
-	    }
-	}
-	
-    int kosaraju(int n, vector<int> adj[])
-    {
-        lv=0;
-        v.resize(n+5);
-        fill(v.begin(),v.end(),0);
-        vv.resize(n);
-        fill(vv.begin(),vv.end(),0);
-        for(ll i=0;i<n;i++){
-            if(v[i]==0){
-                dfs(i,adj);
+    //Function to return a list of lists of integers denoting the members 
+    //of strongly connected components in the given graph.
+    typedef int ll;
+    vector<ll> dis,low,in;
+    stack<ll> stk;
+    ll time;
+    vector<vector<ll>> van;
+    void dfs(ll n,vector<ll> adj[]){
+        stk.push(n);
+        dis[n]=low[n]=time;
+        time++;
+        ll i,j;
+        in[n]=1;
+        for(i=0;i<adj[n].size();i++){
+            if(dis[adj[n][i]]==-1){
+                dfs(adj[n][i],adj);
+                low[n]=min(low[n],low[adj[n][i]]);
             }
-        }
-        vector<ll> edge[n+5];
-        for(ll i=0;i<n;i++){
-            for(ll j=0;j<adj[i].size();j++){
-                edge[adj[i][j]].push_back(i);
-            }
-        }
-        ll s=0;
-        nv.resize(n+5);
-        fill(nv.begin(),nv.end(),0);
-        while(st.size()){
-            ll node=st.top();
-            st.pop();
-            
-            if(nv[node]==1) continue;
-            else{
-                dfs2(node,edge);
-                s++;
+            else if(in[adj[n][i]]==1){
+               
+                low[n]=min(low[n],dis[adj[n][i]]);
             }
         }
         
-        return s;
+        if(low[n]==dis[n]){
+            vector<ll> v;
+            while(stk.top()!=n){
+                v.push_back(stk.top());
+                in[stk.top()]=0;
+                stk.pop();
+            }
+            in[n]=0;
+            v.push_back(n);
+            sort(v.begin(),v.end());
+            van.push_back(v);
+            stk.pop();
+        }
+    }
+    vector<vector<int>> tarjans(int n, vector<int> adj[])
+    {
+        //code here
+        time=0;
+        dis.resize(n);
+        fill(dis.begin(),dis.end(),-1);
+        low.resize(n);
+        fill(low.begin(),low.end(),-1);
+        in.resize(n);
+        fill(in.begin(),in.end(),0);
+        ll i,j;
+        for(i=0;i<n;i++){
+            if(dis[i]==-1){
+                dfs(i,adj);
+            }
+        }
+        sort(van.begin(),van.end());
+        return van;
+        
     }
 };
