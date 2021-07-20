9a93e4cb30f1703f4fd819c68f82ec72bb18945a
MAXIMO278 <48401144+MAXIMO278@users.noreply.github.com>
2021-07-20 21:22:25 +0530

    Update segment_tree_range_sum.cpp

--- segment_tree_range_sum.cpp
+++ segment_tree_range_sum.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 typedef long long ll;
 ll inf=1E9+7;
+ll segtree[10000];
 
-void build(ll segtree[],ll input[],ll low, ll high,ll pos)
+void build(ll input[],ll low, ll high,ll pos)
 {
 	if(low==high)
 	{
 		segtree[pos]=input[low];return;
 	}
 	ll mid=(low+high)/2;
-	build(segtree,input,low,mid,2*pos+1);
-	build(segtree,input,mid+1,high,2*pos+2);
+	build(input,low,mid,2*pos+1);
+	build(input,mid+1,high,2*pos+2);
 	segtree[pos]=segtree[2*pos+1]+segtree[2*pos+2];
 }
 
-int query_sum(ll segtree[],ll qlow,ll qhigh,ll low,ll high,ll pos)
+int query_sum(ll qlow,ll qhigh,ll low,ll high,ll pos)
 {
 	if(qlow<=low && qhigh>=high) //total overlap
 	{
@@ -27,8 +28,18 @@ int query_sum(ll segtree[],ll qlow,ll qhigh,ll low,ll high,ll pos)
 		return 0;
 	}
 	ll mid=(low+high)/2;
-	return(query_sum(segtree,qlow,qhigh,low,mid,2*pos+1)+query_sum(segtree,qlow,qhigh,mid+1,high,2*pos+2));
+	return(query_sum(qlow,qhigh,low,mid,2*pos+1)+query_sum(qlow,qhigh,mid+1,high,2*pos+2));
 	
+}
+void update(ll dif,ll id,ll low,ll high,ll pos){
+    if(id<low || id>high){
+        return;
+    }
+    segtree[pos]=segtree[pos]+dif;
+    if(low==high) return;
+    ll mid=(low+high)/2;
+    update(dif,id,low,mid,2*pos+1);
+    update(dif,id,mid+1,high,2*pos+2);
 }
 
 int main()
@@ -37,15 +48,28 @@ int main()
 	cin>> n;// size of the input array
 	ll input[n];
 	for(i=0;i<n;i++) cin>>input[i];
-	ll segtree[4*n];
+	
 	for(i=0;i<4*n;i++) segtree[i]=0;
 	cin>>q;
-	build(segtree,input,0,n-1,0);
+	build(input,0,n-1,0);
 	while(q--)
 	{
 		ll qlow,qhigh;
 		cin>>qlow>>qhigh;
-		cout<<query_sum(segtree,qlow,qhigh,0,n-1,0)<<endl;
+		cout<<query_sum(qlow,qhigh,0,n-1,0)<<endl;
 	}
+    cout<<"-----"<<endl;
+    cin>>q;
+    while(q--){
+        ll id,nw;
+        cin>>id>>nw;
+        ll dif=nw-input[id];
+        input[id]=nw;
+        update(dif,id,0,n-1,0);
+        ll qlow,qhigh;
+		cin>>qlow>>qhigh;
+		cout<<query_sum(qlow,qhigh,0,n-1,0)<<endl;
+        
+    }
 	
 }
